7fbc3455d3e60503c7f98b0a82fb2185c415c2fd
Sambhav Kothari <sambhavs.email@gmail.com>
2026-09-07 12:35:52 +0100

    Update cert-manager to 1.21.1 and Cloudcoil 0.7 (#1)
    * Update cert-manager to 1.21.1 and Cloudcoil 0.7
    
    * Require released Cloudcoil 0.7.1

--- tests/test_import.py
+++ tests/test_import.py
@@ -6,3 +6,10 @@ import cloudcoil.models.cert_manager as cert_manager
 def test_has_modules():
     modules = list(filter(lambda x: isinstance(x, ModuleType), cert_manager.__dict__.values()))
     assert modules, "No modules found in cert_manager"
+
+
+def test_resource_identity():
+    from cloudcoil.models.cert_manager.v1 import Certificate
+
+    assert Certificate.gvk().api_version == "cert-manager.io/v1"
+    assert Certificate.gvk().kind == "Certificate"
